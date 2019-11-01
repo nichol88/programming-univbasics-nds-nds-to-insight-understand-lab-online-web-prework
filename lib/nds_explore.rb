1e07ff95ead9ac3ae9863c6a53067f2720b717ca
Brian Nicholls <nichol88@gmail.com>
2019-11-01 23:32:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,17 +13,16 @@ def print_first_directors_movie_titles
   # AoHoAoH
   # director list, movies, movie list, details
 
-  puts directors_database[0][:movies][0][:title] # first director, movies, first movie, title
+  # puts directors_database[0][:movies][0][:title] # first director, movies, first movie, title
 
-  stephen_movies = []
 
   # for all movies, list title
 
   i = 0
   while i < stephen_movies.length
-    stephen_movies.push(directors_database[0][:movies][i][:title])
+    puts directors_database[0][:movies][i][:title]
     i += 1
   end
-  stephen_movies.join("\n")
+
 
 end
