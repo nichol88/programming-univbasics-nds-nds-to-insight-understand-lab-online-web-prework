72a3fb5af53f88174f2ed76d1e5297177f16b5af
Brian Nicholls <nichol88@gmail.com>
2019-11-01 23:32:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -19,7 +19,7 @@ def print_first_directors_movie_titles
   # for all movies, list title
 
   i = 0
-  while i < stephen_movies.length
+  while i < directors_database[0][:movies].length
     puts directors_database[0][:movies][i][:title]
     i += 1
   end
